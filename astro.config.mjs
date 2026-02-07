e43e5424180c29db76bcf788510762f01f2fb444
Panda35064 <Panda35064@qq.com>
2026-02-07 15:45:16 +0800

    修改site

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "blog.panda35064.xyz",
+	site: "https://blog.panda35064.xyz/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
